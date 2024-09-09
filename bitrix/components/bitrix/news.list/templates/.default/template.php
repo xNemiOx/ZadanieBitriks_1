62fda0b0503147c6ecb95e1065fb52a477443bd9
xNemiOx <tuman20041@yandex.ru>
2024-09-09 18:35:34 +0700

    success

--- bitrix/components/bitrix/news.list/templates/.default/template.php
+++ bitrix/components/bitrix/news.list/templates/.default/template.php
@@ -1,18 +1,10 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
+
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
+<div class="article-list">
     <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
         <?= $arResult["NAV_STRING"] ?><br/>
     <? endif; ?>
@@ -21,65 +13,24 @@ $this->setFrameMode(true);
         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         ?>
-        <div class="news-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+        <div class="article-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
             <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
                 <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
-                    <div class="news-image-container">
+                    <div class="article-image-container">
                         <div class="container-image">
-                        <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
-                            <img
-                                    class="preview_picture"
-                                    border="0"
-                                    src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                                    width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
-                                    height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
-                                    alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
-                                    title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
-                            />
-                        <? else: ?>
-                            <img
-                                    class="preview_picture"
-                                    border="0"
-                                    src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                                    width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
-                                    height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
-                                    alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
-                                    title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
-                            />
-                        <? endif; ?>
+                            <img class="preview_picture" src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" />
                         </div>
                         <div class="container-title-text">
                             <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
-                                <div class="news-title-overlay"><? echo $arItem["NAME"] ?></div>
+                                <div class="article-title-overlay"><? echo $arItem["NAME"] ?></div>
                             <? endif; ?>
                             <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
-                                <div class="news-text-overlay"><? echo $arItem["PREVIEW_TEXT"]; ?></div>
+                                <div class="article-text-overlay"><? echo $arItem["PREVIEW_TEXT"]; ?></div>
                             <? endif; ?>
                         </div>
                     </div>
                 </a>
             <? endif ?>
-            <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
-                <span class="news-date-time"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
-            <? endif ?>
-            <? foreach ($arItem["FIELDS"] as $code => $value): ?>
-                <small>
-                    <?= GetMessage("IBLOCK_FIELD_" . $code) ?>:&nbsp;<?= $value; ?>
-                </small><br/>
-            <? endforeach; ?>
-            <? foreach ($arItem["DISPLAY_PROPERTIES"] as $pid => $arProperty): ?>
-                <small>
-                    <?= $arProperty["NAME"] ?>:&nbsp;
-                    <? if (is_array($arProperty["DISPLAY_VALUE"])): ?>
-                        <?= implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]); ?>
-                    <? else: ?>
-                        <?= $arProperty["DISPLAY_VALUE"]; ?>
-                    <? endif ?>
-                </small><br/>
-            <? endforeach; ?>
         </div>
     <? endforeach; ?>
-    <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
-        <br/><?= $arResult["NAV_STRING"] ?>
-    <? endif; ?>
 </div>
